b0b41bafcf4aaa68c6d29ff4d35b20daa578234f
gilchristc <57827322+gilchristc@users.noreply.github.com>
2020-12-18 03:24:43 +0800

    removed flask debug toolbar

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'gsee-can-see'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG_TB_INTERCEPT_REDIRECTS = False
     DB_USER = 'admin'
     DB_PASS = '6s33_cAns33!'
     DB_HOST = 'weather-db.cwvrupxzygog.ap-southeast-1.rds.amazonaws.com'
